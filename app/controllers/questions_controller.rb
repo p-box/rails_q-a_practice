3861cd3fcd2874c1aee30e0b11fc649d4dcce9b8
p-box <phako.box@gmail.com>
2023-05-03 09:48:36 +0000

    showアクションにidを元にインスタンスを作成、viewにインスタンスの情報を表示

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,6 +5,9 @@ class QuestionsController < ApplicationController
   
   # 質問詳細ページ表示
   def show
+    # p params[:id]
+    @question = Question.find(params[:id])
+    # p @question
   end
   
   # 質問の作成
@@ -14,7 +17,12 @@ class QuestionsController < ApplicationController
   
   # 質問の登録
   def create
-    p question_params
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBへ保存
+    @question.save
+    # showへリダイレクト
+    redirect_to @question
   end
   
   # 質問の編集
